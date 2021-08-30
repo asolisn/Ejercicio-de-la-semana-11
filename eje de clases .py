41c4f59e7eed57243d125a49eed8f02e90c56761
asolisn <85768831+asolisn@users.noreply.github.com>
2021-08-30 14:48:28 -0500

    Add files via upload
    actualizado

---
+++ eje de clases .py
@@ -0,0 +1,23 @@
+Class Matriz:
+    def __init__(self,matriz):
+        self.matriz = matriz
+
+def ingresar(self,dato):
+    pass
+
+def presentar(self)
+    for fila in range(len(self.matriz)):
+        for col in range(len(self.matriz[fila]))):
+          print("[{}]".format(self.matriz[fila][sol]),end=" ")
+             print()
+def buscar(self)
+        for fila in range(len(self.matriz)):
+             for col in range(len(self.matriz[fila]))):
+if self.matriz[fila][col] == valor:
+break
+
+numeros = [[10,20,30,23],[60,80],[25,35,55],[15,35,55],[5,35,55]]      
+# filas
+# col = numeros[0]
+# print(numeros[0],numeros[0][1])
+# print(col,col[1])
